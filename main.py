900f62ad03501c794b3c01edf2664d785903f6e9
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2023-01-25 19:08:18 +0200

    additional functionality

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from Website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # app.secret_key = "my_secret_key"
+    app.secret_key = "my_secret_key"
     app.run(debug=True)
